8ebf7f1a745220fa93ba9ab99cdb2feb635329ea
施振勛 <chris@ccf.org.tw>
2017-12-21 19:41:40 +0800

    commit Kata Done

--- ExesAndOhs.UnitTests/ExesAndOhsAnalyzer.cs
+++ ExesAndOhs.UnitTests/ExesAndOhsAnalyzer.cs
@@ -5,5 +5,29 @@ namespace ExesAndOhs.UnitTests
     [TestFixture]
     public class ExesAndOhsAnalyzerTests
     {
+        [Test]
+        public void VaildStringHasSameAmountXandO_StringIsEmpty_ReturnTrue()
+        {
+            ExesAndOhsAnalyzer analyzer = new ExesAndOhsAnalyzer();
+
+            var result = analyzer.VaildStringHasSameAmountXandO(string.Empty);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestCase("ooxx", true)]
+        [TestCase("OOXX", true)]
+        [TestCase("xooxx", false)]
+        [TestCase("zpzpzpp", true)]
+        [TestCase("zzoo", false)]
+        [TestCase("ooxXm", true)]
+        public void VaildStringHasSameAmountXandO_InputText_Vaild(string text, bool expected)
+        {
+            ExesAndOhsAnalyzer analyzer = new ExesAndOhsAnalyzer();
+
+            var result = analyzer.VaildStringHasSameAmountXandO(text);
+
+            Assert.AreEqual(expected, result);
+        }
     }
 }
--- ExesAndOhs/ExesAndOhsAnalyzer.cs
+++ ExesAndOhs/ExesAndOhsAnalyzer.cs
@@ -1,6 +1,45 @@
-﻿namespace ExesAndOhs
+﻿using System;
+
+namespace ExesAndOhs
 {
     public class ExesAndOhsAnalyzer
     {
+        public bool VaildStringHasSameAmountXandO(string text)
+        {
+            bool result = false;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                result = true;
+            }
+
+            var textLength = text.Length;
+
+            var oAmount = 0;
+
+            var xAmount = 0;
+
+            for (int i = 0; i < textLength; i++)
+            {
+                var singleChar = text.Substring(i, 1);
+
+                if (singleChar.EndsWith("O", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    oAmount++;
+                }
+
+                if (singleChar.EndsWith("X", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    xAmount++;
+                }
+            }
+
+            if (oAmount == xAmount)
+            {
+                result = true;
+            }
+
+            return result;
+        }
     }
 }
